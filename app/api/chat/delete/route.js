6fec99c603d9ad13088cdca51622ccf97de4db5f
Kumar Tgss <kumar.d@technogenesis.in>
2025-06-24 10:27:58 +0530

    opp

--- app/api/chat/delete/route.js
+++ app/api/chat/delete/route.js
@@ -1,7 +1,7 @@
-import Chat from "@/models/Chat";
+
 import { getAuth } from '@clerk/nextjs/server'; 
 import { NextResponse } from "next/server";
-
+import Chat from '@/app/models/Chat';
 export async function POST(req) {
   try {
     const { userId } = getAuth(req);
